b016ce86007b8f7c2a4c2c49290a1402ce5ecb8a
omgismartinez <martnzomg@gmail.com>
2023-01-06 01:31:01 -0600

    🚀 feat: complete request api

---
+++ pages/api/chatgpt.ts
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from "openai";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${req.body.prompt}\n\n-`,
+    temperature: 0,
+    max_tokens: 1024,
+    top_p: 1,
+    stop: ["\n"],
+  });
+
+  const response = completion.data.choices[0].text;
+  res.status(200).json({ response: response })
+}
